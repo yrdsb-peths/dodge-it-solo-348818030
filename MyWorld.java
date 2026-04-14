ccdb70529a21f9483013e9c2486d19ddec6e7ddd
348818030 <348818030@gapps.yrdsb.ca>
2026-04-14 11:45:18 -0400

    Added Student

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,8 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Student ada = new Student();
+        addObject(ada, 100, 200);
     }
 }
---
+++ Student.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Student here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Student extends Actor
+{
+    /**
+     * Act - do whatever the Student wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(2);
+        
+        if(Greenfoot.mouseClicked(null))
+        {
+            setLocation(100,200);
+        }
+    }
+}
